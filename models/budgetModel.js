0562d63fb5f38fdc151dd1c7d79ddc4c56afaadb
Vivek <itspersonal4vivek@gmail.com>
2024-09-18 04:48:04 +0530

    Updated frontend files after resolving conflicts

--- models/budgetModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const budgetSchema = new mongoose.Schema({
-  description: { type: String, required: true },
-  amount: { type: Number, required: true },
-  date: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model('Budget', budgetSchema);
--- src/services/budgetService.js
+++ src/services/budgetService.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://budget-tracker-frontend-phi.vercel.app/';
+const API_URL = 'http://localhost:5000/api/budget';
 
 
 export const getBudgets = async () => {
