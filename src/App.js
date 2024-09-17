1ee94666687e73841adcb2a0117261954eaadd76
Vivek <itspersonal4vivek@gmail.com>
2024-09-18 04:04:54 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import BudgetForm from './components/BudgetForm';
+import BudgetList from './components/BudgetList';
+import { getBudgets, addBudget, deleteBudget } from './services/budgetService';
 
-function App() {
+const App = () => {
+  const [budgets, setBudgets] = useState([]);
+
+  useEffect(() => {
+    fetchBudgets();
+  }, []);
+
+  const fetchBudgets = async () => {
+    const fetchedBudgets = await getBudgets();
+    setBudgets(fetchedBudgets);
+  };
+
+  const handleAddBudget = async (newBudget) => {
+    const addedBudget = await addBudget(newBudget);
+    setBudgets([...budgets, addedBudget]);
+  };
+
+  const handleDeleteBudget = async (id) => {
+    await deleteBudget(id);
+    setBudgets(budgets.filter((budget) => budget._id !== id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Budget Tracker</h1>
+      <BudgetForm onAddBudget={handleAddBudget} />
+      <BudgetList budgets={budgets} onDeleteBudget={handleDeleteBudget} />
     </div>
   );
-}
+};
 
 export default App;
